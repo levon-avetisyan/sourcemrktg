9e7b628e4c648515ebd22244c89a08dab3ccaab5
Levon <levon.s.avetisyan@gmail.com>
2024-12-23 23:32:57 +0400

    Optimized hero section for mobile view.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Spinner } from './styles/CommonStyles';
 import Home from './pages/Home';
 
